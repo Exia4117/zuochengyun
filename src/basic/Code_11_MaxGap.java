b3ee3d38c6b5e1e4334d4393e4d5d2f8c52d2963
Exia4117 <593663174.bz@gmail.com>
2019-11-26 21:16:42 +0800

    MaxGap

---
+++ src/basic/Code_11_MaxGap.java
@@ -0,0 +1,109 @@
+package basic;
+
+import java.util.Arrays;
+
+public class Code_11_MaxGap {
+
+    public static int maxGap(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        int len = arr.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < len; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        if (min == max) {
+            return 0;
+        }
+        boolean[] hasNum = new boolean[len + 1];
+        int[] maxs = new int[len + 1];
+        int[] mins = new int[len + 1];
+        int bid = 0;
+        for (int i = 0; i < len; i++) {
+            bid = bucket(arr[i], max, min, len);
+//            System.out.print(bid + " ");
+            maxs[bid] = hasNum[bid] ? Math.max(maxs[bid], arr[i]) : arr[i];
+            mins[bid] = hasNum[bid] ? Math.min(mins[bid], arr[i]) : arr[i];
+            hasNum[bid] = true;
+        }
+
+        int res = 0;
+        int lastMax = maxs[0];
+        for (int i = 1; i <= len; i++) {
+            if (hasNum[i]) {
+                res = Math.max(res, mins[i] - lastMax);
+                lastMax = maxs[i];
+            }
+        }
+
+        return res;
+    }
+
+    public static int bucket(long num, long max, long min, long length) {
+        return (int) (((num - min) * (length + 1)) / (max - min));
+    }
+
+    // for test
+    public static int comparator(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int gap = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            gap = Math.max(nums[i] - nums[i - 1], gap);
+        }
+        return gap;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+//            System.out.println(maxGap(arr1));
+            int[] arr2 = copyArray(arr1);
+//            System.out.println(comparator(arr2));
+            if (maxGap(arr1) != comparator(arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
+
+//    public static void main(String[] args) {
+//        int[] arr = {86,39,-78,14,-16,-55,87,53,95,69,-67,-2,-53,41,-89,54};
+//
+//        System.out.println("result" + maxGap(arr));
+//
+//    }
+
+}
